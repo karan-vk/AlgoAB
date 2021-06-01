5fc27aa52eb435c7f01597e4500f1938f85bd4c6
karan-vk <karanvknarayanan@gmail.com>
2021-06-01 12:11:34 +0530

    sum of pair

--- Array-ADT/Sumofpair.cpp
+++ Array-ADT/Sumofpair.cpp
@@ -1,23 +1,17 @@
 // Pair With Sum K.cpp
 #include <iostream>
 
-template <size_t n>
-void findsum(int (&A)[n], int sum)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (A[i] + A[j] == sum)
-			{
-				std::cout << A[i] << "+" << A[j] << "=10" << std::endl;
-			}
-		}
-	}
+template <size_t n> void findsum(int (&A)[n], int sum) {
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (A[i] + A[j] == sum) {
+        std::cout << A[i] << "+" << A[j] << "=10" << std::endl;
+      }
+    }
+  }
 }
 
-int main()
-{
-	int A[] = {6, 3, 8, 10, 16, 7, 5, 2, 9, 14};
-	findsum(A, 13);
+int main() {
+  int A[] = {6, 3, 8, 10, 16, 7, 5, 2, 9, 14};
+  findsum(A, 13);
 }
