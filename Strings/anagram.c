4a1e6cf257837f72973d89996adcff65ab0959dd
karan-vk <karanvknarayanan@gmail.com>
2021-06-04 23:19:18 +0530

    Anagram improved

--- Strings/anagram.c
+++ Strings/anagram.c
@@ -2,40 +2,28 @@
 #include <stdio.h>
 int main(int argc, char const* argv[])
 {
-	char A[] = "medical";
-	char B[] = "decimal";
-	int H[26] = {-1};
-	for(int i = 0; i < 27; i++)
-	{
-		H[i] = 0;
-	}
+	char A[] = "verbose";
+	char B[] = "observe";
+	bool isAnagram = true;
+	int H[26] = {0};
 
-	for(int i = 0; A[i] != 0; ++i)
+	for(int i = 0; A[i] != '\0'; ++i)
 	{
 
 		H[A[i] - 97]++;
 	}
-	bool isAnagram = true;
-	for(int i = 0; B[i] != 0; ++i)
+	int i;
+	for(i = 0; B[i] != '\0'; ++i)
 	{
-		H[B[i] - 97]--;
-	}
-	for(int i = 0; i < 26; ++i)
-	{
-		if(H[i] < 0)
+		H[B[i] - 97] -= 1;
+		if(H[B[i] - 97] < 0)
 		{
-			isAnagram = false;
+			printf("The strings are not anagram\n");
+			return 0;
 		}
 	}
 
-	if(isAnagram)
-	{
-		printf("the given strings are anagram\n");
-	}
-	else
-	{
-		printf("The strings are not anagram\n");
-	}
+	printf("the given strings are anagram\n");
 
 	return 0;
 }
